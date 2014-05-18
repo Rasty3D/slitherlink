9dca565f70fd7250ae60bc7526c259a4cda7fbd5
Rasty3D <rasty3d@gmail.com>
2014-05-18 16:09:56 +0200

    Added docs to understand the indexing

---
+++ lib/slither/include/slither.h
@@ -0,0 +1,14 @@
+/**
+ * @file	slither.h
+ * @author	Jesús Ortiz
+ * @version	1.0
+ * @date	18-05-2014
+ * @brief	Slitherlink main interface
+ */
+
+#ifndef SLITHER_H_
+#define SLITHER_H_
+
+
+
+#endif	/* SLITHER_H_ */
---
+++ lib/slither/src/slither.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file	slither.cpp
+ * @author	Jesús Ortiz
+ * @version	1.0
+ * @date	18-05-2014
+ * @brief	Slitherlink main interface
+ */
+
+/*
+ * INCLUDES
+ */
+
+#include "slither.h"
+
